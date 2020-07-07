1972324c2dce309bab3ab377cce52565a5aa9cc1
jhell85 <jhell85@gmail.com>
2020-07-07 16:17:08 -0700

    remove build folder form git ignore

---
+++ build/precache-manifest.cf0a0bedbe0de8d07a5b6beb7c2c45e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adea6aaf6323ab4d1a9da0a05475cd85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf0487a110d26d62b1e",
+    "url": "/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "6b5e8a2cbeea0ebedabd",
+    "url": "/static/css/main.2c303304.chunk.css"
+  },
+  {
+    "revision": "bcf0487a110d26d62b1e",
+    "url": "/static/js/2.d9411276.chunk.js"
+  },
+  {
+    "revision": "6b5e8a2cbeea0ebedabd",
+    "url": "/static/js/main.5c1434eb.chunk.js"
+  },
+  {
+    "revision": "72979ae2ab860f452a8c",
+    "url": "/static/js/runtime-main.14db5d54.js"
+  }
+]);
