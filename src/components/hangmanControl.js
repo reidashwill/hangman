1c13de968400bd1d051800597a72a34ab3d9be37
Reid Ashwill <reidashwill@gmail.com>
2020-07-07 09:56:44 -0700

    add class component

---
+++ src/components/hangmanControl.js
@@ -0,0 +1,17 @@
+// state entered letters =>array
+// state of lives => number
+// word being guessed => string/array
+// actaul hang man => tbd 5 imgs
+
+import React, { Component } from 'react'
+
+
+export default class hangmanControl extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
